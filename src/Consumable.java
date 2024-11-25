052456c57564823ae85d9ed02318be1c625443ca
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-11-25 08:53:22 +0100

    nthynyhu

--- src/Consumable.java
+++ src/Consumable.java
@@ -1,4 +1,17 @@
-public class Consumable extends Item{
-    public int useMax;
-    public int useCurrent;
+public class Consumable extends Item {
+    public int useMax = 0;
+    public int useCurrent = 0;
+
+    public Consumable(int useMax, String name) {
+        this.useMax = useMax;
+        this.name = name;
+    }
+
+    public void use(Character target) {
+        if (useCurrent < useMax) {
+            System.out.println(target.name + " använder " + this.name);
+            target.hp += 10;
+            useCurrent--;
+        }
+    }
 }
