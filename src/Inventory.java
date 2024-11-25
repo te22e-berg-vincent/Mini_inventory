ab6aacca1d9d3b2e7b93bb99ebb2f2ca397ed1fe
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2024-11-25 09:30:11 +0100

    ̶̘͙̬̼͔̠͎͔̦̱̻̫̄̿͜ś̴̨̖̖͓̲̰̠̼̺̜̏̈́̈́̀͒̈́̈͂̿͘͘̕h̶̢̛͔̹̞̰̳̼͇͐̈́͌̽̓̅̃̑̀̍̚̕͝ͅͅd̶̡͖͚̩̭̊̅́̾͌̏̈́̀̂̒͝͝͠ͅͅb̸̧̢̲̹͇́͊̏̄̈́̌͂͂v̵͈̠̽̔̈́ģ̵̨̼͖̝̦̤͉̐̈̓f̷̤̝̰̰͓͓̰͉̺̙̃̌̌͐̏̍̉͋͊͘̚͝

--- src/Inventory.java
+++ src/Inventory.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 public class Inventory {
     ArrayList<Item> items;
 
-public void Display(){
-
+    public void Display() {
+        System.out.println("Nuvarande föremål är:");
+        for (Item item : items) {
+            System.out.println(item.name);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args){
 
+
+
+
+
+
+
+
         Weapon weapon = new Weapon();
     int attackDamage = weapon.attack();
         System.out.println("Närvskada: " + attackDamage);
